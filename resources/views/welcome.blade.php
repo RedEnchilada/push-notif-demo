dbb5465139b8b13ab4301e7eb81d438da4c6cd67
Timothy Bordelon <red@lyrawearspants.com>
2018-11-02 14:38:23 -0500

    Register service worker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,5 +9,20 @@
         please allow the browser permission for notifications, then use <code>./artisan push:notif</code> to test push
         notifications!
     </p>
+
+    <script>
+        // The URL of the service worker script file. Can control tabs within the subdirectory it's located in or lower,
+        // so put it in the root of the site area it serves.
+        const SERVICE_WORKER_URL = '/service_worker.js';
+
+        (function() {
+            if (!'serviceWorker' in navigator) {
+                console.error('Service workers are not supported');
+                return;
+            }
+
+            navigator.serviceWorker.register(SERVICE_WORKER_URL);
+        })();
+    </script>
 </body>
 </html>
