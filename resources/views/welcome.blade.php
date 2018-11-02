fda3ff2909c213a539cf28c2a69a9cb6048e2741
Timothy Bordelon <red@lyrawearspants.com>
2018-11-02 14:22:00 -0500

    repo setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>sample!</title>
+</head>
+<body>
+    <h1>service worker sample</h1>
+    <p>
+        please allow the browser permission for notifications, then use <code>./artisan push:notif</code> to test push
+        notifications!
+    </p>
+</body>
+</html>
