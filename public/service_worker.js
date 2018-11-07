b354f9fe2bf9a7f4e205044575a50d1346085924
Timothy Bordelon <red@lyrawearspants.com>
2018-11-07 17:52:28 -0600

    Handle push notifications in service worker

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,1 +1,10 @@
 console.log('service worker activate! *sparkle*');
+
+self.addEventListener('push', function(event) {
+    let data = event.data.json();
+
+    let title = data.title;
+    let options = data;
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
